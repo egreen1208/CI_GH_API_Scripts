6d428febf69e519ad387f9bf485c455b3b473d29
Ercell Green <134433969+egreen1208@users.noreply.github.com>
2026-01-05 11:22:55 -0500

    Update fs_repo_branches.sh

--- CI-CD-API-Scripts/Github/fs_repo_branches/fs_repo_branches.sh
+++ CI-CD-API-Scripts/Github/fs_repo_branches/fs_repo_branches.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Get all fs repos
-gh repo list forest-service --limit 2000 --json name > allfsrepos.json
+gh repo list ORGNAME --limit 2000 --json name > allfsrepos.json
 
 
 # Input JSON file
-json_file="allfsrepos.json"
+json_file="FILE_NAME.json"
 
 # Generate a timestamp for the output file name
 timestamp=$(date +"%Y%m%d_%H%M%S")
 
 # CSV file
-csv_file="fs_repo_branches_$timestamp.csv"
+csv_file="FILE_NAME_$timestamp.csv"
 
 # Print CSV header
 echo "reponame,branch_count" > "$csv_file"
@@ -21,7 +21,7 @@ jq -c '.[]' "$json_file" | while read -r obj; do
   reponame=$(echo "$obj" | jq -r '.name')
 
   # Get branch count for the repository
-  branch_count=$(gh api repos/forest-service/"$reponame"/branches --paginate | jq length)
+  branch_count=$(gh api repos/ORGNAME/"$reponame"/branches --paginate | jq length)
 
   # Print repo name and branch count to CSV
   echo "$reponame,$branch_count" >> "$csv_file"
